e79a547c20b756f812f2378701c557f15b1a96b9
Kauã Vinícius <kauaponte@hotmail.com>
2024-11-20 01:08:46 -0300

    Finishing hard code

--- main.c
+++ main.c
@@ -5,12 +5,15 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 #define NTP_SERVER_PORT 123
+#define NTP_TIMESTAMP_DELTA 2208988800U
+#define TIMEOUT_MS 20000
 
 typedef struct
 {
@@ -38,6 +41,8 @@ typedef struct
 // Function to get a setted package
 static ntp_packet *get_Package_Setted()
 {
+    printf("\n[Debug] Creating Package\n");
+
     ntp_packet *packet = (ntp_packet *)malloc(sizeof(ntp_packet));
     packet->li_vn_mode = 0x1B;
     packet->stratum = 0;
@@ -54,10 +59,35 @@ static ntp_packet *get_Package_Setted()
     packet->rxTm_f = 0;
     packet->txTm_s = 0;
     packet->txTm_f = 0;
+    printf("\n[Debug] Package Created\n");
     return packet;
 }
 
-char *get_IP(char *url)
+int create_UDP_Socket(struct sockaddr_in server_addr, char *ip)
+{
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0)
+    {
+        printf("\n[ERROR] Cannot get this Ip\n");
+        return -1;
+    }
+
+    static int timeout = TIMEOUT_MS;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+
+    // REMOVE IT
+    int convert_Ip_Result = inet_pton(AF_INET, ip, &server_addr.sin_addr);
+    if (convert_Ip_Result <= 0)
+    {
+        printf("\n[ERROR] Error at convert IP\n");
+        close(sock);
+        return -1;
+    }
+
+    return sock;
+}
+
+char *get_IP(const char *url)
 {
     // Get IP from URL
     printf("\n[Debug] Getting IP from URL\n");
@@ -70,53 +100,146 @@ char *get_IP(char *url)
 
     if ((err = getaddrinfo(url, NULL, &hints, &res)) != 0)
     {
-        printf("\n[Error] Failed to resolve hostname: %s\n", gai_strerror(err));
-        return 1;
+        printf("\n[ERROR] Failed to resolve hostname: %s\n", gai_strerror(err));
+        return NULL;
     }
 
-    char ipstr[INET_ADDRSTRLEN];
+    char *ipstr = (char *)malloc(INET_ADDRSTRLEN);
+    if (ipstr == NULL)
+    {
+        printf("[ERROR] Memory allocation failed\n");
+        freeaddrinfo(res);
+        return NULL;
+    }
+
     struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
-    inet_ntop(AF_INET, &(addr->sin_addr), ipstr, sizeof(ipstr));
+    inet_ntop(AF_INET, &(addr->sin_addr), ipstr, INET_ADDRSTRLEN);
 
     printf("\n[Debug] %s mapped to %s\n", url, ipstr);
+
     freeaddrinfo(res);
     return ipstr;
 }
 
-int main(void)
+struct sockaddr_in configure_Address()
 {
-    // Get URL
-    char url[2049];
-    printf("Enter the URL to NTP request: ");
-    scanf("%s", url);
-    printf("\n[Debug] Your url: %s\n", url);
-
-    char *ip = get_IP(url);
-
-    // Start UDP Socket
-    printf("\n[Debug] Starting UDP Socket\n");
-    int sockfd;
+    printf("\n[Debug] Setting address to send\n");
     struct sockaddr_in server_addr;
-    socklen_t addr_len = sizeof(server_addr);
-
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        printf("\n[Debug] Cannot get this Ip\n");
-        return 1;
-    }
-    printf("[Debug] Configurating address");
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(NTP_SERVER_PORT); // Converte para ordem de bytes de rede
-    if (inet_pton(AF_INET, "12002", &server_addr.sin_addr) <= 0)
+    server_addr.sin_port = htons(NTP_SERVER_PORT); // To bytes
+    return server_addr;
+}
+
+int send_Message(int sock, ntp_packet *packet, struct sockaddr_in server_addr)
+{
+    ssize_t sent_bytes = sendto(sock, packet, sizeof(ntp_packet), 0,
+                                (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (sent_bytes < 0)
     {
-        close(sockfd);
+        printf("\n[ERROR] Error to send message\n");
+        free(packet);
+        close(sock);
+        return 0;
+    }
+    printf("\n[Debug] Message sent successfully\n");
+    return 1;
+}
+
+void transmit_Message(ntp_packet *packet)
+{
+    printf("\n[Debug] Translating message\n");
+    uint32_t txTm_s = ntohl(*(uint32_t *)(packet + 43));
+    txTm_s -= NTP_TIMESTAMP_DELTA;
+
+    time_t timestamp = (time_t)txTm_s;
+    struct tm *tm_info = localtime(&timestamp);
+
+    char time_str[26];
+    strftime(time_str, sizeof(time_str), "%a %b %d %H:%M:%S %Y", tm_info);
+    printf("Data/hora: %s\n", time_str);
+}
+
+int main(int argc, char *argv[])
+{
+    // Get URL
+    if (argc != 2)
+    {
         return 1;
     }
+    const char *url = argv[1];
 
-    // Recieve Message
+    printf("\n[Debug] Your url: %s\n", url);
 
-    // Format Message
+    // Getting IP address
+    char *ip = get_IP(url);
+    printf("\n[Debug] Mapped to %s\n", ip);
 
+    if (ip == NULL)
+        return 1;
+
+    char attempts = 2;
+
+    while (attempts)
+    {
+        attempts--;
+
+        // Setting address and starting socket
+        printf("\n[Debug] Starting UDP Socket\n");
+        struct sockaddr_in server_addr = configure_Address();
+        int sock = create_UDP_Socket(server_addr, ip);
+
+        if (sock == -1)
+            return 1;
+
+        // Message
+        ntp_packet *packet = get_Package_Setted();
+
+        // Send message
+        if (!send_Message(sock, packet, server_addr))
+        {
+            close(sock);
+            continue;
+        }
+
+        // Receive message with timeout
+        fd_set read_fds;
+        struct timeval timeout;
+        FD_ZERO(&read_fds);
+        FD_SET(sock, &read_fds);
+        timeout.tv_sec = 0;
+        timeout.tv_usec = TIMEOUT_MS * 1000;
+
+        int select_result = select(sock + 1, &read_fds, NULL, NULL, &timeout);
+        if (select_result == -1)
+        {
+            printf("\n[ERROR] Error with select\n");
+            close(sock);
+            return 1;
+        }
+        else if (select_result == 0)
+        {
+            printf("\n[ERROR] Timeout\n");
+            close(sock);
+            continue; // try again
+        }
+
+        socklen_t server_addr_len = sizeof(server_addr);
+        if (recvfrom(sock, packet, sizeof(ntp_packet), 0,
+                     (struct sockaddr *)&server_addr, &server_addr_len) < 0)
+        {
+            printf("\n[ERROR] Failed to receive message\n");
+            close(sock);
+            return 1;
+        }
+
+        printf("\n[Debug] Message received successfully\n");
+
+        close(sock);
+
+        // Without test
+        transmit_Message(packet);
+    }
+    printf("Data/hora: não foi possível contactar servidor");
     return 0;
 }
